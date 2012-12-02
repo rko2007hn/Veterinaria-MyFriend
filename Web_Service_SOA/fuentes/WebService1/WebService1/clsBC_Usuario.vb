7ce4a1e8db8a1f81bccc67e3c081960f6324c94d
omar carpio <ocarpio.carpio@gmail.com>
2012-12-02 13:11:20 -0500

    subiendo las fuentes del logieo SOAP

---
+++ Web_Service_SOA/fuentes/WebService1/WebService1/clsBC_Usuario.vb
@@ -0,0 +1,71 @@
+﻿Public Class clsBC_Usuario
+
+    Dim isOK As New clsBE_Usuario
+
+    Public Function ValidarUsuario(ByVal objBE As clsBE_Usuario) As clsBE_Usuario
+        Try
+            Dim isOK As New clsBE_Usuario
+
+            If objBE.Usuario = "" Then
+                isOK.Mensaje = "INGRESE UN USUARIO"
+                isOK.Exito = False
+
+                Return isOK
+            End If
+
+            If objBE.Clave = "" Then
+                isOK.Mensaje = "INGRESE UNA CLAVE"
+                isOK.Exito = False
+
+                Return isOK
+            End If
+
+            isOK = (New clsDA_Usuario).ValidarUsuario(objBE)
+
+            If isOK.Usuario Is Nothing Then
+                isOK.Mensaje = "EL USUARIO O CLAVE NO SE ENCUENTRAN REGISTRADAS"
+                isOK.Exito = False
+
+                Return isOK
+            End If
+
+            If isOK.Clave Is Nothing Then
+                isOK.Mensaje = "EL USUARIO O CLAVE NO SE ENCUENTRAN REGISTRADAS"
+                isOK.Exito = False
+
+                Return isOK
+            End If
+
+            
+
+
+            If isOK.Usuario.ToUpper <> objBE.Usuario.ToUpper Then
+                isOK.Mensaje = "EL USUARIO NO SE ENCUENTRA REGISTRADO"
+                isOK.Exito = False
+
+                Return isOK
+            End If
+
+            If isOK.Clave.ToUpper <> objBE.Clave.ToUpper Then
+                isOK.Mensaje = "LA CLAVE INGRESA NO ES LA CORRECTA"
+                isOK.Exito = False
+
+                Return isOK
+            End If
+
+            isOK.Mensaje = "BIENVENIDO   " + isOK.Nombre.ToUpper + " " + isOK.Apellido.ToUpper
+            isOK.Exito = True
+
+
+
+            Return isOK
+            ' Return (New clsDA_Usuario).ValidarUsuario(objBE)
+        Catch ex As Exception
+            isOK.Mensaje = "ERROR INESPERADO"
+            Return isOK
+
+        End Try
+    End Function
+
+
+End Class
---
+++ Web_Service_SOA/fuentes/WebService1/WebService1/clsBE_Usuario.vb
@@ -0,0 +1,103 @@
+﻿Public Class clsBE_Usuario
+
+    Private _ID As Integer
+    Public Property ID() As Integer
+        Get
+            Return _ID
+        End Get
+        Set(ByVal value As Integer)
+            _ID = value
+        End Set
+    End Property
+
+    Private _Usuario As String
+    Public Property Usuario() As String
+        Get
+            Return _Usuario
+        End Get
+        Set(ByVal value As String)
+            _Usuario = value
+        End Set
+    End Property
+
+    Private _Clave As String
+    Public Property Clave() As String
+        Get
+            Return _Clave
+        End Get
+        Set(ByVal value As String)
+            _Clave = value
+        End Set
+    End Property
+
+    Private _Apellido As String
+    Public Property Apellido() As String
+        Get
+            Return _Apellido
+        End Get
+        Set(ByVal value As String)
+            _Apellido = value
+        End Set
+    End Property
+
+    Private _Nombre As String
+    Public Property Nombre() As String
+        Get
+            Return _Nombre
+        End Get
+        Set(ByVal value As String)
+            _Nombre = value
+        End Set
+    End Property
+
+    Private _Email As String
+    Public Property Email() As String
+        Get
+            Return _Email
+        End Get
+        Set(ByVal value As String)
+            _Email = value
+        End Set
+    End Property
+
+    Private _Telefono As String
+    Public Property Telefono() As String
+        Get
+            Return _Telefono
+        End Get
+        Set(ByVal value As String)
+            _Telefono = value
+        End Set
+    End Property
+
+    Private _Perfil As String
+    Public Property Perfil() As String
+        Get
+            Return _Perfil
+        End Get
+        Set(ByVal value As String)
+            _Perfil = value
+        End Set
+    End Property
+
+    Private _Mensaje As String
+    Public Property Mensaje() As String
+        Get
+            Return _Mensaje
+        End Get
+        Set(ByVal value As String)
+            _Mensaje = value
+        End Set
+    End Property
+
+    Private _Exito As Boolean
+    Public Property Exito() As Boolean
+        Get
+            Return _Exito
+        End Get
+        Set(ByVal value As Boolean)
+            _Exito = value
+        End Set
+    End Property
+
+End Class
